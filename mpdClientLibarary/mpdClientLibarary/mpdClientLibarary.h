baca2e830bb1f026c2328a545c928754971aa6ac
Mary Beth McWhinney <MegaByte@Mary-Beth-McWhinneys-MacBook-Pro.local>
2013-02-11 15:25:08 -0600

    starting library

---
+++ mpdClientLibarary/mpdClientLibarary/mpdClientLibarary.h
@@ -0,0 +1,13 @@
+//
+//  mpdClientLibarary.h
+//  mpdClientLibarary
+//
+//  Created by Mary Beth McWhinney on 2/11/13.
+//  Copyright (c) 2013 Kyle Hershey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mpdClientLibarary : NSObject
+
+@end
---
+++ mpdClientLibarary/mpdClientLibarary/mpdClientLibarary.m
@@ -0,0 +1,13 @@
+//
+//  mpdClientLibarary.m
+//  mpdClientLibarary
+//
+//  Created by Mary Beth McWhinney on 2/11/13.
+//  Copyright (c) 2013 Kyle Hershey. All rights reserved.
+//
+
+#import "mpdClientLibarary.h"
+
+@implementation mpdClientLibarary
+
+@end
