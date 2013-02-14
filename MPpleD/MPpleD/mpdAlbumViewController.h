90cfcbac102f640fb505066fbd4b26b2bf2ef518
KYLE HERSHEY <kwhershey@SH308-28.local>
2013-02-13 22:09:20 -0600

    created files for my view controllers, added basic buttons, added library.  Seems to compile.  Need to figure out how to access functions.

---
+++ MPpleD/MPpleD/mpdAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  mpdAlbumViewController.h
+//  MPpleD
+//
+//  Created by KYLE HERSHEY on 2/13/13.
+//  Copyright (c) 2013 Kyle Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mpdAlbumViewController : UITableViewController
+
+@end
---
+++ MPpleD/MPpleD/mpdServerSettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  mpdServerSettingsViewController.h
+//  MPpleD
+//
+//  Created by KYLE HERSHEY on 2/13/13.
+//  Copyright (c) 2013 Kyle Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import 
+
+@interface mpdServerSettingsViewController : UIViewController
+
+
+
+@end
